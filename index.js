2d363105aca7177b60db8c7e4a6204184526c285
Drew <drwomble27@gmail.com>
2023-04-11 17:01:42 -0600

    complete

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 // Code your solution in this file!
 
 
-// function distanceFromHqInBlocks(someValue) {
-//     if (someValue > 42){
-//      return someValue - 42;
-//     } 
-//     else {
-//      return 42 - someValue;
-//     }
-//   }
-
 function distanceFromHqInBlocks(someValue) {
-  if (someValue > 42){
-   return someValue - 42;
-  } 
-  else {
-   return 42 - someValue;
+    if (someValue > 42){
+     return someValue - 42;
+    } 
+    else {
+     return 42 - someValue;
+    }
   }
-}
 
 function distanceFromHqInFeet(someValue) {  
-  return distanceFromHqInBlocks((someValue) * 264);
+    return distanceFromHqInBlocks(someValue) * 264;
+
   }
 
+  function distanceTravelledInFeet(start, destination) {
+    if (destination > start){
+      return ((destination - start) * 264);
+    }
+    else {
+      return ((start - destination) * 264);
+    }
+  }
 
-  console.log(distanceFromHqInBlocks(43));
-  console.log(distanceFromHqInBlocks(16));
+  function calculatesFarePrice(start , destination){
+    
+    if (distanceTravelledInFeet(start, destination) < 400){
+      return 0;
+    }
+    else if ((400 < distanceTravelledInFeet(start, destination)) && (2000 > distanceTravelledInFeet(start, destination))){
+      return ((distanceTravelledInFeet(start, destination) - 400) * 0.02);
+    }
+    else if (2000 < distanceTravelledInFeet(start, destination) && 2500 >distanceTravelledInFeet(start, destination)){
+      return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500){
+      return "cannot travel that far";
+    }
+  }
+  
