24ad17a150f79c14051d18e887e2c85e374b8983
Drew <drwomble27@gmail.com>
2023-04-11 16:02:07 -0600

    changes

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+// function distanceFromHqInBlocks(someValue) {
+//     if (someValue > 42){
+//      return someValue - 42;
+//     } 
+//     else {
+//      return 42 - someValue;
+//     }
+//   }
+
+function distanceFromHqInBlocks(someValue) {
+  if (someValue > 42){
+   return someValue - 42;
+  } 
+  else {
+   return 42 - someValue;
+  }
+}
+
+function distanceFromHqInFeet(someValue) {  
+  return distanceFromHqInBlocks((someValue) * 264);
+  }
+
+
+  console.log(distanceFromHqInBlocks(43));
+  console.log(distanceFromHqInBlocks(16));
